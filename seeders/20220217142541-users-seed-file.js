17d3ce7c34d502fcda8eb5664c222517ce819051
ReoNaBear <89585478+ReoNaBear@users.noreply.github.com>
2022-02-19 00:24:50 +0800

    create users/tweets/replies seeder

--- seeders/20220217142541-users-seed-file.js
+++ seeders/20220217142541-users-seed-file.js
@@ -1,26 +1,31 @@
 'use strict';
+const faker = require('faker')
+const bcrypt = require('bcrypt-nodejs')
+
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Users',
+      Array.from({ length: 6 }).map((user, i) => ({
+        id: i == 1 ? i : (i * 10 + 1),
+        account: i == 1 ? 'root' : 'user' + (i - 1),
+        email: i == 1 ? 'root@example.com' : `user${i - 1}@example.com`,
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        //name上限50字 可以放在create or edit時的限制
+        name: faker.name.findName(),
+        avatar: `https://loremflickr.com/320/240/selfie?lock=${i}`,
+        introduction: faker.lorem.text().substring(0, 160),
+        role: i == 1 ? 'admin' : 'user',
+        //是不是還有個背景圖?
+        cover: `https://loremflickr.com/400/300/landscape?lock=${i}`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }))
+    )
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {})
   }
-};
+}
