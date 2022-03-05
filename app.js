df356444b4155828dab04f48aaf6c99d788644d1
ronnychiang <ronnychiang1164@gmail.com>
2022-03-05 17:02:38 +0800

    socket io setting

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 
 
 app.use(cors())
@@ -17,9 +22,23 @@ app.use(passport.initialize())
 
 
 require('./routes')(app)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/app.html');
+});
 app.get('/', (req, res) => res.send('Hello World!'))
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+});
 
 module.exports = app
